9f4f7daad8df93c45782f8daf9e2beed3aafd35e
MatthewFavela <mfavela3508@myhta.org>
2019-01-17 14:41:51 -1000

    post done

--- public/post/post.js
+++ public/post/post.js
@@ -1,15 +1,41 @@
-window.onload = function(){
-    const movieTitle = document.querySelector("#title");
-    const yearReleased = document.querySelector("#released");
-    const genre = document.querySelector("#genre");
-    const form = document.querySelector("#form");
 
 
-    form.addEventListener("submit", function(){
-        event.preventDefault();
-        localStorage.setItem("title", movieTitle.value);
-        localStorage.setItem("released", yearReleased.value);
-        localStorage.setItem("genre", genre.value);
-        console.log(localStorage);
-    });
-}
+    window.onload = ()=>{
+
+        const movieTitle = document.querySelector("#title");
+        const yearReleased = document.querySelector("#released");
+        const genre = document.querySelector("#genre");
+        const form = document.querySelector("#form");
+        const submitButton = document.getElementById('submit-button')
+        console.log(form)
+        
+        
+        
+        
+        // const data = {
+            //         movie_title: 'hasdlasld',
+            //         movie_genre:' genre.va',
+            //         movie_year_released: 44
+            //     }
+            
+            submitButton.addEventListener("click", function(){
+                event.preventDefault();
+                const data = {
+                    movie_title: movieTitle.value,
+                    movie_genre: genre.value,
+                    movie_year_released: Number(yearReleased.value)
+                }
+                console.log('movieTitle.value', movieTitle.value);
+                console.log('genre.value', genre.value);
+                console.log('yearReleased.value', Number(yearReleased.value));
+                axios.post(`https://0i6v8ygy8b.execute-api.us-west-2.amazonaws.com/dev/postMovie`, data)
+                .then(function (response) {
+                
+                    window.location.href = '/index.html'
+                })
+                .catch(function (err) {
+                    console.log('Errors = None // ', err);
+                })
+            })
+            
+        }
